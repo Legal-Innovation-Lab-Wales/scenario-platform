d4b5d02958dadc8415ee55d67001fd5ae17cb8ba
phil-6 <github@purpleriver.dev>
2021-01-07 19:05:59 +0000

    update quiz migration

--- db/migrate/001_create_quizzes.rb
+++ db/migrate/001_create_quizzes.rb
@@ -2,6 +2,12 @@ class CreateQuizzes < ActiveRecord::Migration[6.1]
   def change
     create_table :quizzes do |t|
 
+      t.belongs_to :user
+
+      t.string :variables, array: true, default: []
+      t.string :name
+      t.text :description
+
       t.timestamps
     end
   end
--- spec/factories/quizzes.rb
+++ spec/factories/quizzes.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :quiz do
-    
+    variables { "MyString" }
+    name { "MyString" }
+    description { "MyText" }
   end
 end
