01a76e8c6b91ce602a9a7c5a83f4bee92f5335bd
phil-6 <github@purpleriver.dev>
2021-01-07 19:10:13 +0000

    add foreign key to quiz association

--- db/migrate/001_create_quizzes.rb
+++ db/migrate/001_create_quizzes.rb
@@ -2,7 +2,7 @@ class CreateQuizzes < ActiveRecord::Migration[6.1]
   def change
     create_table :quizzes do |t|
 
-      t.belongs_to :user
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.string :variables, array: true, default: []
       t.string :name
