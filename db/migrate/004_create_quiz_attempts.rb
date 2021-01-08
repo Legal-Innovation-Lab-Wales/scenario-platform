0f9dc9f1207f507fdeb0df6957468c6c8e7e5ea3
phil-6 <github@purpleriver.dev>
2021-01-08 08:47:54 +0000

    add hstore extenstion

--- db/migrate/004_create_quiz_attempts.rb
+++ db/migrate/004_create_quiz_attempts.rb
@@ -1,5 +1,6 @@
 class CreateQuizAttempts < ActiveRecord::Migration[6.1]
   def change
+    enable_extension 'hstore' unless extension_enabled?('hstore')
     create_table :quiz_attempts do |t|
       t.belongs_to :quiz, null: false, foreign_key: true
       t.belongs_to :user, null: false, foreign_key: true
