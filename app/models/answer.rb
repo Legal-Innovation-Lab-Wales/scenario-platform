1783b168f0e0864184295d0d035d2df9b01b5785
phil-6 <github@purpleriver.dev>
2021-01-11 10:45:53 +0000

    remove next_question_ID from answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
-  belongs_to :question, foreign_key: 'next_question_ID'
+
 end
--- db/migrate/003_create_answers.rb
+++ db/migrate/003_create_answers.rb
@@ -2,12 +2,11 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     enable_extension 'hstore' unless extension_enabled?('hstore')
     create_table :answers do |t|
-      t.belongs_to :question, null: false, foreign_key: true
       t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :question, null: false, foreign_key: true
       t.string :text
       t.hstore :variable_mods
       t.integer :next_question_order
-      t.integer :next_question_ID
 
       t.timestamps
     end
