6635efc25beb41e3498a8ae2b039f09cd36002a2
phil-6 <github@purpleriver.dev>
2021-01-14 12:57:27 +0000

    roughly add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,18 @@ Rails.application.routes.draw do
   get 'pages/user_profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  #resources :quizzes
-  get '/quizzes', to: 'quizzes#index'
+  devise_for :users,  path: '',
+                      path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile'},
+                      controllers: {
+                        registrations: 'users/registrations',
+                        sessions: 'users/sessions',
+                        confirmations: 'users/confirmations'
+                      }
+  devise_scope :user do
+    get '/confirmation/pending', to: 'users/confirmations#pending'
+  end
+  
+  resources :quizzes
+  #get '/quizzes', to: 'quizzes#index'
 
 end
