4d8a5a06e22ea057a255badf25a32ff45012af9a
phil-6 <github@purpleriver.dev>
2021-01-16 14:33:33 +0000

    get quiz navigation working with question and answer displays

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,24 @@
 class QuestionsController < ApplicationController
+
+  before_action :set_question, only: [:show]
+
+  def show
+    @question
+
+
+
+    respond_to do |format|
+      format.html
+      # TODO Improve this query
+      format.json { render json: @question.as_json(include: :answers), status: :ok }
+    end
+
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
 end
