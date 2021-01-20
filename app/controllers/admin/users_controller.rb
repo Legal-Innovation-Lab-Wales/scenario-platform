7843d4b4d447cfbbc2009bad68f828ba54adf494
phil-6 <github@purpleriver.dev>
2021-01-20 10:32:09 +0000

    update admin users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,28 @@
-class Admin::UsersController < ApplicationController
-  before_action :authorize_admin
-  before_action :set_user, except: :index
+module Admin
+  class UsersController < ApplicationController
+    before_action :authorize_admin
+    # before_action :set_user, except: :index
 
-  def index
-    render template: "admin/users"
-  end
+    def index
+      @users = User.all.where(organisation: current_user.organisation)
+      render template: 'admin/users'
+    end
 
-  def authorize_admin
-    redirect_to(root_path) unless current_user && current_user.admin?
-  end
+    private
 
-  def set_user
-    @user = User.find(params[:id])
+    def authorize_admin
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+    # def set_user
+    #   @user = User.find(params[:id])
+    # end
+
+    def account_update_params
+      params.require(:user).permit(:first_name,
+                                   :last_name,
+                                   :email,
+                                   :admin)
+    end
   end
 end
