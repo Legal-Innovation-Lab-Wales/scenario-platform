09a6a0d317a80eaa9e152dc705ab1a6bba3a744e
phil-6 <github@purpleriver.dev>
2021-01-21 12:23:37 +0000

    [QUIZ-13] routing specs for quizzes, questions and answers

--- spec/routing/answers_routing_spec.rb
+++ spec/routing/answers_routing_spec.rb
@@ -1,25 +1,26 @@
-require "rails_helper"
+# spec/routing/answers_routing_spec.rb
+require 'rails_helper'
 
 RSpec.describe AnswersController, type: :routing do
-  describe "routing" do
-    it "routes to #new" do
-      expect(get: "/answers/new").to route_to("answers#new")
+  describe 'routing' do
+    it 'routes to #new' do
+      expect(get: '/quizzes/1/questions/1/answers/new').to route_to('answers#new', quiz_id: '1', question_id: '1')
     end
 
-    it "routes to #edit" do
-      expect(get: "/answers/1/edit").to route_to("answers#edit", id: "1")
+    it 'routes to #edit' do
+      expect(get: '/quizzes/1/questions/1/answers/1/edit').to route_to('answers#edit', quiz_id: '1', question_id: '1', id: '1')
     end
 
-    it "routes to #create" do
-      expect(post: "/answers").to route_to("answers#create")
+    it 'routes to #create' do
+      expect(post: '/quizzes/1/questions/1/answers/').to route_to('answers#create', quiz_id: '1', question_id: '1')
     end
 
-    it "routes to #update via PUT" do
-      expect(put: "/answers/1").to route_to("answers#update", id: "1")
+    it 'routes to #update via PUT' do
+      expect(put: '/quizzes/1/questions/1/answers/1').to route_to('answers#update', quiz_id: '1', question_id: '1', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/answers/1").to route_to("answers#destroy", id: "1")
+    it 'routes to #destroy' do
+      expect(delete: '/quizzes/1/questions/1/answers/1').to route_to('answers#destroy', quiz_id: '1', question_id: '1', id: '1')
     end
   end
 end
