c6c9f98abe6e56102ddebc4b0521ff74c6530903
phil-6 <github@purpleriver.dev>
2021-01-21 11:19:53 +0000

    [QUIZ-13] update answer model spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of(:text) }
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:question_id) }
+
+  it 'updates the next question id' do
+    answer = Answer.new
+    expect answer.next_question_id.present?
+  end
 end
