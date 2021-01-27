28613f82271ca0a69e952350bc6795b6e84da5ee
phil-6 <github@purpleriver.dev>
2021-01-27 08:23:45 +0000

    QUIZ-58 Update Organisation, only method is show
    Organsistions will need to be set up via command line, users should sign up with hidden form field

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,74 +1,76 @@
+# app/controllers/organisations_controller.rb
 class OrganisationsController < ApplicationController
-  before_action :set_organisation, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: :show
+  before_action :set_organisation # , only: [:show, :edit, :update, :destroy]
 
   # GET /organisations
   # GET /organisations.json
-  def index
-    @organisations = Organisation.all
-  end
+  # def index
+  #   @organisations = Organisation.all
+  # end
 
   # GET /organisations/1
   # GET /organisations/1.json
-  def show
-  end
+  def show; end
 
   # GET /organisations/new
-  def new
-    @organisation = Organisation.new
-  end
-
-  # GET /organisations/1/edit
-  def edit
-  end
-
-  # POST /organisations
-  # POST /organisations.json
-  def create
-    @organisation = Organisation.new(organisation_params)
-
-    respond_to do |format|
-      if @organisation.save
-        format.html { redirect_to @organisation, notice: 'Organisation was successfully created.' }
-        format.json { render :show, status: :created, location: @organisation }
-      else
-        format.html { render :new }
-        format.json { render json: @organisation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def new
+  #   @organisation = Organisation.new
+  # end
+  #
+  # # GET /organisations/1/edit
+  # def edit
+  # end
+  #
+  # # POST /organisations
+  # # POST /organisations.json
+  # def create
+  #   @organisation = Organisation.new(organisation_params)
+  #
+  #   respond_to do |format|
+  #     if @organisation.save
+  #       format.html { redirect_to @organisation, notice: 'Organisation was successfully created.' }
+  #       format.json { render :show, status: :created, location: @organisation }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @organisation.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /organisations/1
   # PATCH/PUT /organisations/1.json
-  def update
-    respond_to do |format|
-      if @organisation.update(organisation_params)
-        format.html { redirect_to @organisation, notice: 'Organisation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @organisation }
-      else
-        format.html { render :edit }
-        format.json { render json: @organisation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /organisations/1
-  # DELETE /organisations/1.json
-  def destroy
-    @organisation.destroy
-    respond_to do |format|
-      format.html { redirect_to organisations_url, notice: 'Organisation was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @organisation.update(organisation_params)
+  #       format.html { redirect_to @organisation, notice: 'Organisation was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @organisation }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @organisation.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  #
+  # # DELETE /organisations/1
+  # # DELETE /organisations/1.json
+  # def destroy
+  #   @organisation.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to organisations_url, notice: 'Organisation was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
-    def set_organisation
-      @organisation = Organisation.find(params[:id])
-    end
+  def set_organisation
+    @organisation = Organisation.find(params[:id])
+  end
 
     # Only allow a list of trusted parameters through.
-    def organisation_params
-      params.require(:organisation).permit(:name)
-    end
+  # def organisation_params
+  #   params.require(:organisation).permit(:name)
+  # end
 end
--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -2,4 +2,7 @@
 class Organisation < ApplicationRecord
   has_many :users, foreign_key: :organisation_id
   has_many :quizzes, through: :users
+
+  # validations
+  validates_presence_of :name
 end
