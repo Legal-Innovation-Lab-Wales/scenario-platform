04443c9fb7eacc176318e132e7f02ce34eceeda0
phil-6 <github@purpleriver.dev>
2021-01-31 19:04:46 +0000

    [QUIZ-52] Update answers form

--- app/javascript/packs/erb_form_functions.js
+++ app/javascript/packs/erb_form_functions.js
@@ -4,16 +4,14 @@ $('#remove_variable').on('click', remove_variable_input);
 
 function add_variable_input() {
     var new_var_no = parseInt($('#total_vars').val()) + 1;
-    var new_var_input = "<span id='variable-" + new_var_no +
-        "'><input multiple='multiple' type='text' name='quiz[variables][]'></span>";
-    $('#variables').append(new_var_input);
+    var new_input = "<tr id='variable-" + new_var_no +
+        "'><td><input multiple='multiple' type='text' name='quiz[variables][]'></td>" +
+        "<td><input multiple='multiple' type='text' name='quiz[variable_initial_values][]'></td></tr>" ;
+
+    $('#variables').append(new_input);
+
     $('#total_vars').val(new_var_no);
 
-    var new_val_no = parseInt($('#total_vals').val()) + 1;
-    var new_val_input = "<span id='initial_value-" + new_val_no +
-        "'><input multiple='multiple' type='text' name='quiz[variable_initial_values][]'></span>";
-    $('#initial_values').append(new_val_input);
-    $('#total_vals').val(new_val_no);
 }
 
 function remove_variable_input() {
@@ -24,10 +22,4 @@ function remove_variable_input() {
         $('#total_vars').val(last_var_no - 1);
     }
 
-    var last_val_no = $('#total_vals').val();
-
-    if (last_val_no > 1) {
-        $('#initial_value-' + last_val_no).remove();
-        $('#total_vals').val(last_val_no - 1);
-    }
 }
