9accec8811964f92554392076ce501a08723f210
Ieuan Skinner <ieuanskinner@gmail.com>
2021-02-12 14:17:04 +0000

    QUIZ-101 Added an alert notice for when the rails backend redirects a user either because they don't have a valid attempt (and so need to start/resume one) or when they do have an attempt but try to skip to a question they shouldn't.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -78,13 +78,13 @@ class QuestionsController < ApplicationController
 
   def verify_attempt
     if @quiz_attempt.nil? || @quiz_attempt.completed
-      redirect_to quiz_path(@quiz)
+      redirect_to quiz_path(@quiz), notice: "You need to start or resume a quiz to view its questions"
     end
   end
 
   def verify_question
     if @question.id != @quiz_attempt.next_question_id && !@quiz_attempt.has_been_answered(@question.id)
-      redirect_to quiz_question_path(@quiz, @quiz_attempt.next_question_id)
+      redirect_to quiz_question_path(@quiz, @quiz_attempt.next_question_id), notice: "This is the question you should be answering"
     end
   end
 
