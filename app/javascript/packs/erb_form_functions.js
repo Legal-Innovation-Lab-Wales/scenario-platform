8cdb7200ce5ff072fdcf400a5408fc993df5762f
Ieuan Skinner <ieuanskinner@gmail.com>
2021-02-26 12:36:29 +0000

    SP-82 Updated ERB functions to remove JQuery in favour of pure JS methods and reset the webpack environment config as a result.

--- app/javascript/packs/erb_form_functions.js
+++ app/javascript/packs/erb_form_functions.js
@@ -1,21 +1,12 @@
-$('#add_variable').on('click', () => {
-  $('#variables').append(
-    `<tr>
-      <td>
-        <input multiple='multiple' type='text' name='scenario[variables][]' class='form-control'>
-      </td>
-      <td>
-        <input multiple='multiple' type='number' name='scenario[variable_initial_values][]' class='form-control'>
-      </td>
-      <td>
-        <button type="button" class="btn btn-sm btn-danger remove_variable">Remove</button>
-      </td>
-    </tr>`
-  )
-
-  $('.remove_variable').on('click', e => e.target.closest('tr').remove())
-})
-$('.remove_variable').on('click', e => e.target.closest('tr').remove())
+const table = document.getElementById('variables')
+const form = document.getElementById('scenario_form')
+const add_btn = document.getElementById('add_variable')
+const attempt_count = document.getElementById('attempt_count')
+const initial_variables = get_variables()
+const initial_values = get_values()
+const warning_message = `Warning! You are changing the variables for a scenario that has past attempts.\r\n
+Changing these variables will not affect the scores of these past attempts but it will affect the score for all future attempts.\r\n
+If you are aware of this outcome and happy to proceed click OK below.`
 
 function get_variables() {
   return JSON.stringify([...document.getElementsByName('scenario[variables][]')].map(variable => variable.value))
@@ -25,23 +16,31 @@ function get_values() {
   return JSON.stringify([...document.getElementsByName('scenario[variables][]')].map(variable => variable.value))
 }
 
-const initial_variables = get_variables()
-const initial_values = get_values()
+function add_remove_listener() {
+  document.querySelectorAll('.remove_variable').forEach(
+      remove_btn => remove_btn.addEventListener('click', e => e.target.closest('tr').remove()))
+}
 
-$("#scenario_form").submit(function(e) {
-  e.preventDefault();
+add_btn.addEventListener('click', () => {
+  const new_row = table.insertRow(-1), variable = new_row.insertCell(0),
+        variable_initial_value = new_row.insertCell(1), remove_btn = new_row.insertCell(2)
 
-  const attempt_count = parseInt($('#attempt_count').val())
+  variable.innerHTML = `<input multiple='multiple' type='text' name='scenario[variables][]' class='form-control'>`
+  variable_initial_value.innerHTML = `<input multiple='multiple' type='number' name='scenario[variable_initial_values][]' class='form-control'>`
+  remove_btn.innerHTML = `<button type="button" class="btn btn-sm btn-danger remove_variable">Remove</button>`
 
-  if (attempt_count > 0 && (initial_variables !== get_variables() || initial_values !== get_values())) {
-    if (confirm(`Warning! You are changing the variables for a scenario that has past attempts.\r\n
-Changing these variables will not affect the scores of these past attempts but it will affect the score for all future attempts.\r\n
-If you are aware of this outcome and happy to proceed click OK below.`)) {
-      this.submit()
-    } else {
+  add_remove_listener()
+})
+
+form.addEventListener('submit', e => {
+  if (parseInt(attempt_count.value) > 0 && (initial_variables !== get_variables() || initial_values !== get_values())) {
+    if (!confirm(warning_message)) {
+      e.preventDefault()
       return false
     }
-  } else {
-    this.submit()
   }
+
+  return true
 })
+
+add_remove_listener()
