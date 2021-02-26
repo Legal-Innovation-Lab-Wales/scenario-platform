45ee9a888890c4c0f67e2342aaf6ed62cf69be8b
Ieuan Skinner <ieuanskinner@gmail.com>
2021-02-26 12:12:23 +0000

    SP-82 Warning dialogue now appears to let the user know the risks involved with changing a set of variables for a scenario that already has attempts.

--- app/javascript/packs/erb_form_functions.js
+++ app/javascript/packs/erb_form_functions.js
@@ -1,47 +1,47 @@
+$('#add_variable').on('click', () => {
+  $('#variables').append(
+    `<tr>
+      <td>
+        <input multiple='multiple' type='text' name='scenario[variables][]' class='form-control'>
+      </td>
+      <td>
+        <input multiple='multiple' type='number' name='scenario[variable_initial_values][]' class='form-control'>
+      </td>
+      <td>
+        <button type="button" class="btn btn-sm btn-danger remove_variable">Remove</button>
+      </td>
+    </tr>`
+  )
 
-$('#add_variable').on('click', add_variable_input);
-$('#remove_variable').on('click', remove_variable_input);
-
-function add_variable_input() {
-    var new_var_no = parseInt($('#total_vars').val()) + 1;
-    var new_input = "<tr id='variable-" + new_var_no +
-        "'><td><input multiple='multiple' type='text' name='scenario[variables][]' class='form-control'></td>" +
-        "<td><input multiple='multiple' type='number' name='scenario[variable_initial_values][]' class='form-control'></td></tr>" ;
-
-    $('#variables').append(new_input);
-
-    $('#total_vars').val(new_var_no);
-
-}
-
-function remove_variable_input() {
-    var last_var_no = $('#total_vars').val();
-
-    if (last_var_no > 1) {
-        $('#variable-' + last_var_no).remove();
-        $('#total_vars').val(last_var_no - 1);
-    }
-
-}
+  $('.remove_variable').on('click', e => e.target.closest('tr').remove())
+})
+$('.remove_variable').on('click', e => e.target.closest('tr').remove())
 
 function get_variables() {
-    return [...document.getElementsByName('scenario[variables][]')].map(variable => variable.value)
+  return JSON.stringify([...document.getElementsByName('scenario[variables][]')].map(variable => variable.value))
 }
 
 function get_values() {
-    return [...document.getElementsByName('scenario[variable_initial_values][]')].map(value => value.value)
+  return JSON.stringify([...document.getElementsByName('scenario[variables][]')].map(variable => variable.value))
 }
 
 const initial_variables = get_variables()
 const initial_values = get_values()
 
-$("#scenario-form").submit(function(e) {
-    e.preventDefault();
+$("#scenario_form").submit(function(e) {
+  e.preventDefault();
 
-    if ((initial_variables.length > 0 || initial_values.length > 0) &&
-        (initial_variables !== get_variables() || initial_values !==  get_values())) {
-        console.log('Initial variables and/or values have changed')
+  const attempt_count = parseInt($('#attempt_count').val())
+
+  if (attempt_count > 0 && (initial_variables !== get_variables() || initial_values !== get_values())) {
+    if (confirm(`Warning! You are changing the variables for a scenario that has past attempts.\r\n
+Changing these variables will not affect the scores of these past attempts but it will affect the score for all future attempts.\r\n
+If you are aware of this outcome and happy to proceed click OK below.`)) {
+      this.submit()
+    } else {
+      return false
     }
-
+  } else {
     this.submit()
-});
+  }
+})
